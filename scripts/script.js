25a583dc667b4e183da2f8c6d3f2168331b830fa
Jack Ryan Oracoy <jackryanoracoy@gmail.com>
2019-08-22 13:38:03 +0800

    Update: Made Fix and changes to Flexbox utilities.

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,16 @@
 // IE support for "main"
 document.createElement('main');
 
-
 // Object-Fit
 $(function () { objectFitImages() });
-
 
 // Add target="_blank" rel="noreferrer noopener"
 $('a[href^="http://"], a[href^="https://"]').attr({ target:"_blank", rel:"noreferrer noopener" });
 
-
 // Immersive
 $(document).ready(function($) {
   var lastScroll = 0;
-    $(window).scroll(function(){
+    $(window).scroll(function() {
     setTimeout(function() {
       var scroll = $(window).scrollTop();
       if (scroll > lastScroll + 10) {
@@ -27,10 +24,36 @@ $(document).ready(function($) {
       } else {
         $(".l-site-header").removeClass("l-site-header--active");
       } lastScroll = scroll;
-    }, 120);
+    }, 300);
   });
 });
 
+// Smooth scroll
+$(document).ready(function() {
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({ scrollTop: $(hash).offset().top }, 300,
+      function() { window.location.hash = hash; });
+    }
+  });
+});
+
+// Change font-size with button
+$(document).ready(function($) {
+  $('#font-large').click(function() {
+    $("html").css("font-size", "75%");
+    $("#font-large").addClass("c-button--secondary");
+    $("#font-normal").removeClass("c-button--secondary");
+  });
+
+  $('#font-normal').click(function() {
+    $("html").css("font-size", "62.5%");
+    $("#font-normal").addClass("c-button--secondary");
+    $("#font-large").removeClass("c-button--secondary");
+  });
+});
 
 // Toggle class on click
 $(document).ready(function($) {
@@ -41,23 +64,6 @@ $(document).ready(function($) {
   });
 });
 
-
-// Smooth scroll
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 300, function(){
-        window.location.hash = hash;
-      });
-    }
-  });
-});
-
-
 // Detect if user is using TAB to navigate
 function handleFirstTab(e) {
   if (e.keyCode === 9) {
@@ -67,9 +73,8 @@ function handleFirstTab(e) {
 }
 window.addEventListener('keydown', handleFirstTab);
 
-
 // Parallax
-$('.u-parallax__image').each(function(){
+$('.u-parallax__image').each(function() {
   var img = $(this);
   var imgParent = $(this).parent();
   function parallaxImg () {
@@ -78,6 +83,7 @@ $('.u-parallax__image').each(function(){
     var winY = $(this).scrollTop();
     var winH = $(this).height();
     var parentH = imgParent.innerHeight();
+
 
     // The next pixel to show on screen
     var winBottom = winY + winH;
@@ -97,7 +103,24 @@ $('.u-parallax__image').each(function(){
     });
   }
   $(document).on({
-    scroll: function () { parallaxImg(); },
-    ready: function () { parallaxImg(); }
+    scroll: function () {
+      parallaxImg();
+    }, ready: function () {
+      parallaxImg();
+    }
   });
+});
+
+// Tabs
+$(document).ready(function() {
+
+	$('.u-tab-link li').click(function() {
+		var tab_id = $(this).attr('data-tab');
+
+		$('.u-tab-link li').removeClass('current');
+		$('.u-tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
 });
