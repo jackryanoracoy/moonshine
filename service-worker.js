cde97202f2cb3061d71b242cf18492f53ab26fad
Jack Ryan Oracoy <jackryanoracoy@gmail.com>
2019-08-26 16:52:35 +0800

    Update: Creating Quick Start Guide (WIP).

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,64 @@
-// This is the "Offline page" service worker
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-const CACHE = "Front-end-Template";
+const CACHE = "front-end-template-offline-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
-  console.log("Service Worker: Install Event processing");
+  console.log("[Front-end Template] Install Event processing");
 
   event.waitUntil(
     caches.open(CACHE).then(function (cache) {
-      console.log("Service Worker: Cached offline page during install");
+      console.log("[Front-end Template] Cached offline page during install");
 
-      if (offlineFallbackPage === "offline.html") {
+      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
         return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-      }
-
-      return cache.add(offlineFallbackPage);
+      } return cache.add(offlineFallbackPage);
     })
   );
 });
 
-// If any fetch fails, it will show the offline page.
+// If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener("fetch", function (event) {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request).catch(function (error) {
-      // The following validates that the request was for a navigation to a new document
-      if (
-        event.request.destination !== "document" ||
-        event.request.mode !== "navigate"
-      ) {
-        return;
-      }
+    fetch(event.request)
+    .then(function (response) {
+      console.log("[Front-end Template] add page to offline cache: " + response.url);
 
-      console.error("Service Worker: Network request Failed. Serving offline page " + error);
-      return caches.open(CACHE).then(function (cache) {
-        return cache.match(offlineFallbackPage);
-      });
+      // If request was success, add or update it in the cache
+      event.waitUntil(updateCache(event.request, response.clone()));
+
+      return response;
+    })
+    .catch(function (error) {
+      console.log("[Front-end Template] Network request Failed. Serving content from cache: " + error);
+      return fromCache(event.request);
     })
   );
 });
 
-// This is an event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener("refreshOffline", function () {
-  const offlinePageRequest = new Request(offlineFallbackPage);
-
-  return fetch(offlineFallbackPage).then(function (response) {
-    return caches.open(CACHE).then(function (cache) {
-      console.log("Service Worker: Offline page updated from refreshOffline event: " + response.url);
-      return cache.put(offlinePageRequest, response);
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return the offline page
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        // The following validates that the request was for a navigation to a new document
+        if (request.destination !== "document" || request.mode !== "navigate") {
+          return Promise.reject("no-match");
+        } return cache.match(offlineFallbackPage);
+      } return matching;
     });
   });
-});
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
